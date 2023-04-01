feeeaa7ba7eb0d617764b2ec9133496a7eed585c
Krishna <mkvyas999@gmail.com>
2023-04-01 12:48:42 +0800

    propagate the file_name

--- train_contrastive.py
+++ train_contrastive.py
@@ -63,7 +63,7 @@ class custom_transformers_trainer():
             
             # save the model after 25 epochs
             if epoch % 25 == 0 and epoch!=0:
-                self.save_model(f"transformer_weights_{epoch}.pt")
+                self.save_model(f"transformer_weights_{self.train_dataset.filename}.pt")
                 
             print(f"Epoch: {epoch}, Loss: {loss_total/total_steps}")
 
@@ -83,4 +83,4 @@ if __name__ == '__main__':
         
     trainer.train()
     # path to save the weights. 
-    trainer.save_model('transformer_weights_final.pt')
+    trainer.save_model(f'transformer_weights_final_{filename}.pt')
